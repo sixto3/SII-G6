bbcbd2ac65ad03bb275ddcc8aac5745b39108f35
brahwlex <102476563+brahwlex@users.noreply.github.com>
2022-04-24 15:53:22 +0100

    Add files via upload
    Excepciones nuevas+anteriores

---
+++ g6/sii/g6/g6-ejb/src/main/java/exceptions/AutorizadoExistenteException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class AutorizadoExistenteException extends AdministracionException{
+
+}
---
+++ g6/sii/g6/g6-ejb/src/main/java/exceptions/ClienteExistenteException.java
@@ -0,0 +1,6 @@
+package exceptions;
+
+public class ClienteExistenteException extends AdministracionException  {
+
+}
+	
